d291e5dd68e00d2baf9dd23a55abc0d652e48473
Nazar <nazarsikander@yahoo.com>
2014-06-17 22:26:42 -0700

    finally added user auth like a BOSS, still needs tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140617185432) do
+ActiveRecord::Schema.define(version: 20140618050318) do
 
   create_table "statuses", force: true do |t|
     t.string   "name"
     t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "profile_name"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
